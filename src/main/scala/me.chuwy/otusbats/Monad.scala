d158e9b8ef4f76fdead3212884e401f4f26fd555
Tigran Manasyan <manasyan_tigran@mail.ru>
2024-04-11 00:45:40 +0400

    Homework

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -5,9 +5,55 @@ trait Monad[F[_]] extends Functor[F] { self =>
 
   def point[A](a: A): F[A]
 
-  def flatten[A](fa: F[F[A]]): F[A]
+  def flatten[A](fa: F[F[A]]): F[A] = {
+    flatMap(fa)(identity)
+  }
 }
 
 object Monad {
+  def apply[F[_]](implicit instance: Monad[F]): Monad[F] = instance
 
+  implicit class ValueOps[A, F[_]](value: A) {
+    def point(implicit monad: Monad[F]): F[A] = monad.point(value)
+  }
+
+  implicit class MonadOps[A, F[_]](container: F[A]) {
+    def flatMap[B](f: A => F[B])(implicit monad: Monad[F]): F[B] = {
+      monad.flatMap(container)(f)
+    }
+
+    def map[B](f: A => B)(implicit monad: Monad[F]): F[B] = {
+      monad.map(container)(f)
+    }
+  }
+
+  implicit class NestedMonadOps[A, F[_]](container: F[F[A]]) {
+    def flatten(implicit monad: Monad[F]): F[A] = monad.flatten(container)
+  }
+
+  implicit val optionMonad: Monad[Option] = new Monad[Option] {
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+    override def point[A](a: A): Option[A] = Option(a)
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+  }
+
+  implicit val listMonad: Monad[List] = new Monad[List] {
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+    override def point[A](a: A): List[A] = List(a)
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  // kind-projector
+  implicit def eitherMonadKind[L]: Monad[Either[L, *]] = new Monad[Either[L, *]] {
+    override def flatMap[A, B](fa: Either[L, A])(f: A => Either[L, B]): Either[L, B] = fa.flatMap(f)
+    override def point[A](a: A): Either[L, A] = Right(a)
+    override def map[A, B](fa: Either[L, A])(f: A => B): Either[L, B] = fa.map(f)
+  }
+
+  // type lambdas
+  def eitherMonad[L]: Monad[({type l[R] = Either[L, R]})#l] = new Monad[({type l[R] = Either[L, R]})#l] {
+    override def flatMap[A, B](fa: Either[L, A])(f: A => Either[L, B]): Either[L, B] = fa.flatMap(f)
+    override def point[A](a: A): Either[L, A] = Right(a)
+    override def map[A, B](fa: Either[L, A])(f: A => B): Either[L, B] = fa.map(f)
+  }
 }
--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -9,20 +9,23 @@ object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
 
+  implicit def intShow: Show[Int] = _.toString
+  implicit def stringShow: Show[String] = identity
+  implicit def booleanShow: Show[Boolean] = _.toString
 
   // 1.2 Instances with conditional implicit
 
   implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+    (a: List[A]) => mkString_(a, "[", "]", ",")
 
 
   // 2. Summoner (apply)
+  def apply[A](implicit instance: Show[A]): Show[A] = instance
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
-    def show(implicit ev: Show[A]): String =
-      ???
+    def show(implicit ev: Show[A]): String = ev.show(a)
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -39,16 +42,17 @@ object Show {
    *  @param begin. '[' in above example
    *  @param end. ']' in above example
    */
-  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    ???
+  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String = {
+    list.map(_.show)
+      .mkString(begin, separator, end)
+  }
 
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
-  
-  /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromJvm[A]: Show[A] = _.toString
 
+  /** Provide a custom function to avoid `new Show { ... }` machinery */
+  def fromFunction[A](f: A => String): Show[A] = (a: A) => f(a)
 }
